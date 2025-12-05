5737ec90dcc962a2fa4b248c6c6848d9ce1cceb4
irfanmkh <irfankhakiki17@gmail.com>
2025-12-05 19:53:09 +0700

    fix pencarian angka hilang & add readme

---
+++ soal-3/cari_angka.php
@@ -0,0 +1,36 @@
+<?php
+
+function cariAngka($arr)
+{
+    $min = min($arr);
+    $max = max($arr);
+    $hilang = [];
+
+
+
+    for ($i = $min; $i <= $max; $i++) {
+        if (!in_array($i, $arr)) {
+            $hilang[] = $i;
+        }
+    }
+
+
+    if (empty($hilang)) {
+        return "Lengkap, tidak ada angka yang hilang";
+    } else {
+        return implode(", ", $hilang);
+    }
+}
+
+
+$input = readline("Masukkan angka (pisahkan dengan koma): ");
+
+// ubah ke array
+$inputArray = explode(',', $input);
+// ubah ke integer 
+$inputArray = array_map('intval', $inputArray);
+// hilangkan duplikat
+$unik = array_unique($inputArray);
+
+echo "\nInput : [" . implode(", ", $unik) . "]\n";
+echo "Angka / Urutan yang hilang: " . cariAngka($unik) . "\n";
---
+++ soal-3/readme.md
@@ -0,0 +1,7 @@
+## CARA PENGGUNAAN
+
+jalankan perintah di terminal
+
+```
+php cari_angka.php
+```
